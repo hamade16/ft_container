2cb3268e48b9e5f9a49c20084f180927bb78b382
Hamad Oubeid <houbeid@e1r1p4.1337.ma>
2022-08-05 16:25:08 +0100

    all

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
-#include <vector>
+//#include "ft_containers.hpp"
 #include <memory>
+#include <vector>
 
 using namespace std;
 int main()
 {
-    allocator<int> myAllocator;
-    int *p = myAllocator.allocate(10);
+    //allocator<int> myAllocator;
+    //ft::vector<int> ar(10); 
+    std::vector<int> ar;
 
-    int i= 0;
-    while (i < 10)
+    ar.push_back(1);
+    ar.push_back(-6);
+    ar.push_back(8);
+    ar.push_back(9);
+    ar.push_back(4);
+
+    std::vector <int>::iterator it = ar.begin();
+    std::vector<int>::reverse_iterator rit = ar.rbegin();
+    while (rit != ar.rend())
     {
-        myAllocator.construct(i+p, 0);
-        cout << p[i] << endl;
-        i++;
+        std::cout << *rit << std::endl;
+        rit++;
     }
-    system("leaks a.out");
 }
---
+++ random_access_iterator.hpp
@@ -0,0 +1,12 @@
+#ifndef RANDOM_ACCESS_ITERATOR_HPP
+# define RANDOM_ACCESS_ITERATOR_HPP
+
+#include <iostream>
+
+template<class T>
+class random_access_iterator{
+    
+}
+
+
+#endif
