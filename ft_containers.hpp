daf1cab6fdee80ad59486123e869ad11a660be54
Hamad Oubeid <houbeid@e1r1p12.1337.ma>
2022-08-16 01:50:06 +0100

    all

--- ft_containers.hpp
+++ ft_containers.hpp
@@ -9,35 +9,75 @@ namespace ft
 {
     template<class T, class Alloc = std::allocator<T>() >
     class vector {
-       /*typedef T value_type;
-        typedef T* pointer;*/
-        typedef Alloc allocator_type;
         typedef T                                        value_type;
-        //typedef allocator                                allocator_type;
+        typedef std::allocator<value_type>               allocator_type;
         typedef typename allocator_type::reference       reference;
         typedef typename allocator_type::const_reference const_reference;
         typedef typename allocator_type::pointer         pointer;
         typedef typename allocator_type::const_pointer   const_pointer;
-        typedef typename random_access_iterator<pointer>          iterator;
+       // typedef typename random_access_iterator<pointer>          iterator;
         typedef std::size_t size_type;
         //typedef ft::reverse_iterator<iterator>          reverse_iterator;
         //typedef ft::reverse_iterator<const_iterator>    const_reverse_iterator; 
 
-
-
-        //typedef typename Alloc::pointer pointer;
-        //typedef typename Alloc::reference reference;
-       // typedef Alloc       allocat_type;
-
-        //allocat_type        my_alloc;
         private:
             size_type _capacite;
             size_type _size;
             size_type _max_size;
-            pointer vector;
+            pointer _vector;
             allocator_type all;
 
         public:
+            explicit vector (const allocator_type& alloc = allocator_type())
+            {
+                all = alloc;
+                _capacite = 0;
+                _size = 0;
+                _max_size = all.max_size();
+                _vector  = NULL;
+            }
+            explicit vector (size_type n, const value_type& val = value_type(),
+                 const allocator_type& alloc = allocator_type())
+            {
+                all = alloc;
+                _capacite = n;
+                _size = n;
+                _max_size = all.max_size();
+                _vector = all.allocate(n);
+                for(int i = 0; i < n; i++)
+                    all.construct(_vector+i, val);
+            }
+            
+            vector (const vector& x)
+            {
+                this->all = x.all;
+                this->_vector = all.allocate(x._capacite);
+                this->_capacite = x._capacite;
+                this->_size = x._size;
+                this->_max_size = x._max_size;
+                for(int i = 0; i < x._size; i++)
+                    this->all.construct(this->_vector+i, x._vector[i]);
+            }
+
+            vector& operator= (const vector& x)
+            {
+                if (this->_capacite > 0)
+                    this->all.deallocate(this->_vector, this->_capacite);
+                this->all = x.all;
+                this->_vector = all.allocate(x._capacite);
+                this->_capacite = x._capacite;
+                this->_size = x._size;
+                this->_max_size = x._max_size;
+                for(int i = 0; i < x->_size; i++)
+                    this->all.construct(this->_vector+i, x._vector+i);
+                return (*this);
+            }
+
+            reference operator[] (size_type n)
+            {
+               return *(this->_vector+n);
+            }
+
             size_type capacity() const
             {
                 return _capacite;
@@ -46,9 +86,9 @@ namespace ft
             {
                 return (_size);
             }
-            size_type max_size() const;
+            size_type max_size() const
             {
-                return _max_size;
+                return (_max_size);
             }
             void resize (size_type n, value_type val = value_type())
             {
@@ -56,32 +96,79 @@ namespace ft
                     _size = n;
                 else if (n > _size && n < _capacite)
                 {
-                    for(int i = _size, i < n, i++)
-                        all.construct(vector + i , val);
+                    for(int i = _size; i < n; i++)
+                        all.construct(_vector + i , val);
 
                     _size = n;
                 }
                 else if (n > _size && n > _capacite)
                 {
                     _capacite *= 2;
-                    if (_capcite < n)
+                    if (_capacite < n)
                         _capacite = n;
                     pointer v;
                     v = all.allocate(_capacite);
                     for (size_t i = 0; i < _size; i++)
-                        all.construct(v + i, vector[i]);
+                        all.construct(v + i, _vector+i);
                     for(size_t i = _size; i < n; i++)
                       all.construct(v + i, val);
+                    all.deallocate (_vector, _capacite);
                 }
             }
-            bool empty() const;
+            bool empty() const
             {
                 if (_size == 0)
                     return true;
                 else
-                    false;
+                    return false;
+            }
+            void reserve (size_type n)
+            {
+                if (n > _capacite)
+                {
+                    pointer v;
+                    v = all.allocate(n);
+                    _capacite = n;
+                    for(int i = 0; i < _size; i++)
+                        all.construct(v + i, *(_vector+i));
+                }
             }
             
+            void push_back (const value_type& val)
+            {
+                size_t old_capacity = 0;
+                if (this->_size < this->_capacite)
+                {
+                    this->all.construct(this->_vector+_size, val);
+                }
+                else
+                {
+                    old_capacity = _capacite;
+                    _capacite *= 2;
+                    pointer v;
+                    v = all.allocate(this->_capacite);
+                    int i;
+                    for (i = 0; i < this->_size; i++)
+                    {
+                        all.construct(v+i, *(this->_vector+i));
+                        all.destroy(this->_vector + i);
+                    }
+                    all.deallocate(this->_vector, old_capacity);
+                    all.construct(v+i, val);
+                    // old_vector
+                    this->_vector = v;
+                    // new memory _vector
+                }
+            }
+            void pop_back()
+            {
+                if (_size > 0)
+                {
+                    all.destroy(this->_vector+(_size - 1));
+                    _size -= 1;
+
+                }
+            }
         /*vector(T a)
         {
             arr = all.allocate(10);
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include "ft_containers.hpp"
+#include "ft_containers.hpp"
 #include <memory>
 #include <vector>
 
@@ -8,19 +8,26 @@ int main()
 {
     //allocator<int> myAllocator;
     //ft::vector<int> ar(10); 
-    std::vector<int> ar;
+    ft::vector<int> ar(5, 2);
+    ft::vector<int> x(ar);
+    ar.pop_back();
+    //ar.push_back(5);
+    for(int i = 0; i < ar.size(); i++)
+        std::cout << ar[i] << std::endl;
+   // system("leaks a.out");
 
-    ar.push_back(1);
-    ar.push_back(-6);
-    ar.push_back(8);
-    ar.push_back(9);
-    ar.push_back(4);
 
-    std::vector <int>::iterator it = ar.begin();
-    std::vector<int>::reverse_iterator rit = ar.rbegin();
-    while (rit != ar.rend())
-    {
-        std::cout << *rit << std::endl;
-        rit++;
-    }
+    //ar.push_back(1);
+    //ar.push_back(-6);
+    //ar.push_back(8);
+    //ar.push_back(9);
+    //ar.push_back(4);
+
+    //std::vector <int>::iterator it = ar.begin();
+    //std::vector<int>::reverse_iterator rit = ar.rbegin();
+    //while (rit != ar.rend())
+    //{
+    //    std::cout << *rit << std::endl;
+    //    rit++;
+    //}
 }
