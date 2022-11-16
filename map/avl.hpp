90bdd89bf2c31379af3ab4ef6b419a803cfa6aac
oubeid hammade <houbeid@MacBook-Pro-de-oubeid.local>
2022-11-16 13:27:20 +0100

    all

--- map/avl.hpp
+++ map/avl.hpp
@@ -1,63 +1,175 @@
-#pragma once 
+#ifndef AVL_HPP
+# define AVL_HPP
 
 
 #include <iostream>
-
-template<class T>
-class Node
+#include <functional>
+namespace ft
 {
-    public:
-        T *val;
-        Node *left;
-        Node *right;
-        size_t hight;
-        Node   *parent;   
-};
 
-template<class T>
-class avl
-{
-    typedef T value_type;
-    Node<value_type> node;
-    public:
-    Node* new_node(value_type key)
+    template<class T1, class T2>
+    class pair 
     {
-        Node* newNode(int key)
-        Node* node = new Node();
-        node->key = key;
-        node->left = NULL;
-        node->right = NULL;
-        node->height = 1; // new node is initially
-                          // added at leaf
-        return(node);
-    }
+        public: 
+            typedef T1 first_type;
+            typedef T2 seconde_type;
+        public: 
+            first_type first;
+            seconde_type seconde;
+            pair(): first(), seconde()
+            {
 
-    size_t height(Node *node)
+            }
+            pair(pair<T1,T2> &pr): first(pr.first), seconde(pr.seconde)
+            {
+
+            }
+            pair(first_type x, seconde_type y) : first(x), seconde(y)
+            {
+
+            }
+    };
+
+    template<typename pair>
+    class Node
     {
-        if (node == NULL)
-            return 0;
-        else
-            return (node->height);
-    }
+        public:
+            Node(){
 
-    value_type max(value_type a, value_type b)
+            };
+            ~Node(){
+
+            };
+            pair *data;
+            Node<pair> *left;
+            Node<pair> *right;
+            Node<pair> *parent;
+            size_t height;
+            int balance_factor;
+    };
+
+    template<class T, class key, class Compare = std::less<key>, class Alloc = std::allocator<ft::pair<key,T> > >
+    class avl
     {
-        return (a > b ? a : b); 
-    }
+        public:
+            typedef T mapped_type;
+            typedef key key_type;
+            typedef Alloc allocator_type;
+            typedef ft::pair<key_type, mapped_type> value_type;
+            typedef Node<value_type> Node;
+        private: 
+            allocator_type _all;
+            Compare _compare;
+        public:
+            Node *a;
+            avl(){
+                a = NULL;
+            }
+            ~avl()
+            {
 
-    Node* insert(Node* node, value_type key)
-    {
-        if (node == NULL)
-          return (new_node(key));
-        if (key < node->key)
-            node->left = insert(node->left, key);
-        else if (key > node->key)
-            node->right = insert(node->right, key);
-        else 
-            return (node);
-        node->height = 1 + max(node->left->height, node->right->height);
-        
+            }
+            Node *new_node(value_type x)
+            {
+                //node* newNode(int key);
+                Node* node1;
+                node1->data = _all.allocate(1);
+                _all.construct(node1->data, x);
+                node1->left = NULL;
+                node1->right = NULL;
+                node1->parent = NULL;
+                node1->height = 1; // new node is initially
+                                  // added at leaf
+                node1->balance_fator = 0;                  
+                return(node1);
+            }
 
-    }
-};
+            size_t height(Node *node)
+            {
+                if (node == NULL)
+                    return 0;
+                else
+                    return (node->height);
+            }
+
+            value_type max(value_type a, value_type b)
+            {
+                return (a > b ? a : b); 
+            }
+
+            size_t  getbalance(Node *node)
+            {
+                if (node == NULL)
+                    return 0;
+                else
+                    return (height(node->left) - height(node->right));
+            }
+
+            Node*   lRrotate(Node* y)
+            {
+                Node *tmp = y->left;
+                tmp = leftrotate(tmp);
+                return Rightrotate(y);
+            }
+
+            Node*   Rightrotate(Node* y)
+            {
+                /*Node *x = y->left;
+                Node *z = x->right;
+
+                y->left = z;
+                y->left->left = x;
+                Node *t = llRotate(y);*/
+                Node *tmp = y->left;
+                tmp->right = y;
+                tmp->parent = y->parent;
+                y->parent = tmp;
+                return tmp;
+            }
+
+            Node* leftrotate(Node* y)
+            {
+               Node* tmp = y->right;
+               tmp->left = y;
+               tmp->parent = y->parent;
+               y->parent = tmp;
+               return (tmp);
+            }
+
+            Node* RLrotate(Node* y)
+            {
+                Node *tmp = y->right;
+                tmp = Rightrotate(tmp);
+                return (leftrotate);
+            }
+
+            Node* insert(Node* node, value_type x)
+            {
+                if (node == NULL)
+                {
+                  return (new_node(x));
+                }
+                if (x.first < node->data->first)
+                    node->left = insert(node->left, x);
+                else if (x > node->key)
+                    node->right = insert(node->right, x);
+                else 
+                    return (node);
+                node->height = 1 + max(height(node->left), height(node->right));
+                node->balance_factor = getbalance(node);
+
+                if (node->balance_factor < -1 && x < node->right->key)
+                    return(RLrotate(node));
+                if (node->balance_factor < -1 && x > node->right->key)
+                    return (RRrotate(node));
+                if (node->balance_factor > 1 && x < node->left->key)
+                    return (llrotate(node));
+                if (node->balance_factor > 1 && x > node->left->key)
+                    return (lRrotate(node));
+
+                return (node);
+            }
+    };
+}
+#endif
 
--- map/main.cpp
+++ map/main.cpp
@@ -5,13 +5,26 @@
 int main()
 {
     // Node<int÷÷> node(10);
-    std::map<int, char> map;
+    // std::map<int, char> map;
 
-    std::pair<int, char> val(1337, 'H');
+    // std::pair<int, char> val(1337, 'H');
     
-    map.insert(val);
+    // map.insert(val);
     
-    std::cout << map.at(1337);
+    // std::cout << map.at(1337);
+    // avl<int> tree;
+    // tree.a  = tree.insert(tree.a, 2);
+    std::map
+    avl<int> root;
+     
+    /* Constructing tree given in
+    the above figure */
+    root.insert(root.a, 10);
+    root.insert(root.a, 20);
+    root.insert(root.a, 30);
+    root.insert(root.a, 40);
+    root.insert(root.a, 50);
+    root.insert(root.a, 25);
 
 
     return 0;
