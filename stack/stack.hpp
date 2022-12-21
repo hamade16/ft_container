e2057cf016244ade08d365e2b29de880b7abfcdc
Hamad Oubeid <houbeid@e1r11p2.1337.ma>
2022-12-21 20:03:36 +0100

    stack

--- stack/stack.hpp
+++ stack/stack.hpp
@@ -4,12 +4,19 @@
 #include <iostream>
 #include "../vector.hpp"
 
-template <class T, class Container = deque<T> >
+template <class T, class Container = ft::vector<T> >
 class stack
 {
     public:
-        typedef T       value_type;
-        
+        typedef T           value_type;
+        typedef Container   container_type;
+        typedef size_t      size_type;
+
+
+        explicit stack (const container_type& ctnr = container_type())
+        {
+            
+        }
 }
 
 #endif
