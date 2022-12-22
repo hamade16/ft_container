7173659c385e76f5348436ecf1f74909c336b849
Mustapha <mouassit@student.1337.ma>
2022-12-22 11:59:24 +0100

    tester

--- map/main.cpp
+++ map/main.cpp
@@ -11,7 +11,7 @@
 
 int main()
 {
-      std::map<int, std::string> m3;
+     std::map<int, std::string> m3;
         ft::map<int, std::string> ft_m3;
         std::vector<int> vec;
         std::vector<int> ft_vec;
@@ -19,13 +19,13 @@ int main()
         std::mt19937 generator(randDev());
         std::uniform_int_distribution<int> distr(0, 1e8);
 
-         for (size_t i = 0; i < 1e6; i++)
+        for (size_t i = 0; i < 1e6; i++)
         {
             m3.insert(std::make_pair(i, "string1"));
             ft_m3.insert(ft::make_pair(i, "string1"));
         }
 
-        for (size_t i = 0; i < 1e5; ++i)
+        for (size_t i = 0; i < 1e6; ++i)
         {
             int n = distr(generator);
             int ret1 = m3.erase(n);
@@ -33,10 +33,11 @@ int main()
 
             if (ret1 != ret2)
             {
-                //cond = false;
+               //cond = false;
                 break;
             }
         }
+
         if (!m3.empty())
         {
             m3.erase(m3.begin(), m3.end());
@@ -45,9 +46,10 @@ int main()
         if (!ft_m3.empty())
         {
             ft_m3.erase(ft_m3.begin(), ft_m3.end());
-             //exit(0);
             ft_m3.erase(ft_m3.begin(), ft_m3.end());
-            //exit(0);
         }
+        //cond = cond && (m3.size() == ft_m3.size() && compareMaps(m3.begin(), m3.end(), ft_m3.begin(), ft_m3.end()));
+
+       // EQUAL(con
     return 0;
 }
